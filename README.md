9c9fe5dab533973f21531350fa1e294ca106a31d
serweryn617 <seweryn.r.1999@gmail.com>
2024-07-18 18:51:10 +0200

    More updates

--- README.md
+++ README.md
@@ -10,6 +10,16 @@
 
 Firmware can be built using [cook-builder](https://github.com/serweryn617/cook):
 
+**Setup:** initialize submodules and build Docker image. Has to be run only once:
+
+```bash
+cook setup
+```
+
+**Build:** build everything. Output files will be copied to the repository top level:
+
 ```bash
 cook
 ```
+
+For complete list of commands run `cook -i`.
--- recipe.py
+++ recipe.py
@@ -5,21 +5,24 @@ import os
 from pathlib import Path
 
 
-def docker_command(command):
+def docker_command(command=None, interactive=False):
     return ' '.join(f'''
-        docker run --rm -it
+        docker run --rm
+        {'-it' if interactive else ''}
         -u {os.getuid()}:{os.getgid()}
         -v {Path.cwd() / 'firmware'}:/workspace/firmware
         -v {Path.cwd() / 'sdk'}:/workspace/sdk
         -v {Path.cwd() / 'build'}:/workspace/build
         -e PICO_SDK_PATH=/workspace/sdk/pico-sdk
-        pico-builder
-        /bin/bash -c "{command}"
+        -w /workspace
+        pico_builder
+        /bin/bash
+        {'' if interactive else f'-c "{command}"'}
     '''.split())
 
 
 default_build_server = 'local'
-default_project = 'build_all'
+default_project = 'build'
 
 
 build_servers = [
@@ -33,37 +36,51 @@ projects = {}
 projects['setup'] = {
     'components': [
         'init_submodules',
-        'build_docker',
+        'build_docker_image',
     ],
 }
 
 
-projects['build_all'] = {
+projects['build'] = {
     'components': [
-        'firmware',
+        'build_firmware_docker',
         'copy_compile_commands',
     ],
 }
 
 
-projects['build_firmware'] = {
+projects['init_submodules'] = local_build_from_list([
+    ('sdk', 'git submodule update --init --depth 1 pico-sdk'),
+    'git submodule status',
+
+    ('sdk/pico-sdk/lib', 'git submodule update --init --depth 1 tinyusb'),
+    'git submodule status',
+])
+
+
+projects['build_docker_image'] = local_build_from_list([
+    'docker build -t pico_builder .'
+])
+
+
+projects['build_firmware_docker'] = {
     'build_servers': [
         LocalBuildServer(),
     ],
 
     'build_steps': [
         BuildStep(command='mkdir -p build/generated/pio'),
+
         BuildStep(command=docker_command("cd /workspace/build && cmake ../firmware -DSIDE=0 && cmake --build . -j")),
+        BuildStep(command='cp build/keypad.uf2 keypad_left.uf2'),
+
+        BuildStep(command=docker_command("cd /workspace/build && cmake ../firmware -DSIDE=1 && cmake --build . -j")),
+        BuildStep(command='cp build/keypad.uf2 keypad_right.uf2'),
     ],
 }
 
 
-projects['docker_interactive'] = local_build_from_list([
-    docker_command('')[:-6]
-])
-
-
-projects['firmware'] = local_build_from_list([
+projects['build_firmware'] = local_build_from_list([
     'mkdir -p build',
     ('build', 'mkdir -p generated/pio'),
 
@@ -78,21 +95,11 @@ projects['firmware'] = local_build_from_list([
 
 
 projects['copy_compile_commands'] = local_build_from_list([
-    'cp build/compile_commands.json .vscode/compile_commands.json'
+    'cp build/compile_commands.json .vscode/compile_commands.json',
+    ('.vscode', f'sed -i "s#/workspace#{Path.cwd()}#g" compile_commands.json'),
 ])
 
 
-projects['init_submodules'] = local_build_from_list([
-    ('sdk', 'git submodule update --init --depth 1 pico-sdk'),
-    'git submodule status',
-
-    ('sdk/pico-sdk/lib', 'git submodule update --init --depth 1 tinyusb'),
-    'git submodule status',
+projects['docker_interactive'] = local_build_from_list([
+    docker_command(interactive=True)
 ])
-
-
-projects['build_docker'] = local_build_from_list([
-    'docker build -t pico-builder .'
-])
-
-
