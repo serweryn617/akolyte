c62a11e852b5bdc4aabb04360117a2e8a715883a
serweryn617 <seweryn.r.1999@gmail.com>
2024-10-20 13:37:19 +0200

    Remove top row

--- firmware/lib/defs/inc/defs/defs.hpp
+++ firmware/lib/defs/inc/defs/defs.hpp
@@ -4,7 +4,7 @@
 #include "hardware/i2c.h"
 #include <array>
 
-constexpr uint num_in_pins = 5;
+constexpr uint num_in_pins = 4;
 constexpr uint num_out_pins = 6;
 constexpr uint num_keys = num_in_pins * num_out_pins;
 static_assert(num_keys <= 32, "Number of keys must be <=32 as uint32_t is used to keep state information.");
@@ -13,7 +13,7 @@ constexpr uint i2c_baudrate = 200'000;
 
 #if SIDE == 0 // left
 
-constexpr std::array<uint, num_in_pins> keypad_in_pins = { 8, 6, 7, 5, 4 };
+constexpr std::array<uint, num_in_pins> keypad_in_pins = { 6, 7, 5, 4 };
 constexpr std::array<uint, num_out_pins> keypad_out_pins = { 13, 14, 15, 12, 11, 10 };
 
 constexpr uint ext_i2c_scl = 3;
@@ -28,7 +28,7 @@ constexpr uint led_builtin = 16;
 
 #elif SIDE == 1 // right
 
-constexpr std::array<uint, num_in_pins> keypad_in_pins = { 11, 12, 10, 13, 14 };
+constexpr std::array<uint, num_in_pins> keypad_in_pins = { 12, 10, 13, 14 };
 constexpr std::array<uint, num_out_pins> keypad_out_pins = { 4, 5, 6, 7, 8, 9 };
 
 constexpr uint ext_i2c_scl = 3;
