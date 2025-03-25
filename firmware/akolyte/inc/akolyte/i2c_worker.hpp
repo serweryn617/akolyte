3653c77d29d0ef1ba440f61bdccc6b3409349b2d
serweryn617 <seweryn.r.1999@gmail.com>
2025-03-25 17:28:59 +0100

    Worker work

--- firmware/akolyte/inc/akolyte/i2c_worker.hpp
+++ firmware/akolyte/inc/akolyte/i2c_worker.hpp
@@ -8,6 +8,10 @@ class i2c_worker
 private:
     lib::communication::communication &comms;
     lib::keypad::Keypad &keypad;
+    uint32_t state = 0;
+
+    void process_commands();
+    bool process_slave_request();
 
 public:
     i2c_worker(lib::communication::communication &_comms, lib::keypad::Keypad &_keypad);
--- firmware/akolyte/src/i2c_worker.cpp
+++ firmware/akolyte/src/i2c_worker.cpp
@@ -9,27 +9,36 @@ i2c_worker::i2c_worker(communication &_comms, Keypad &_keypad)
 {
 }
 
+void i2c_worker::process_commands() {
+    command cmd = comms.get_command();
+    if (cmd == command::capture_keys) {
+        state = keypad.get_state();
+    }
+}
+
+bool i2c_worker::process_slave_request() {
+    bool requested = comms.slave_requested();
+    if (requested) {
+        comms.slave_write_byte((state >> 0) & 0xff);
+        comms.slave_write_byte((state >> 8) & 0xff);
+        comms.slave_write_byte((state >> 16) & 0xff);
+        comms.slave_write_byte((state >> 24) & 0xff);
+
+        comms.clear_slave_request();
+    }
+    return requested;
+}
+
 void i2c_worker::loop()
 {
     uint64_t last_reqest_time = time_us_64();
-    uint32_t state = 0;
 
     // TODO: assert i2c is set to slave mode
     while (true)
     {
-        command cmd = comms.get_command();
-        if (cmd == command::capture_keys) {
-            state = keypad.get_state();
-        }
+        process_commands();
 
-        if (comms.slave_requested()) {
-
-            comms.slave_write_byte((state >> 0) & 0xff);
-            comms.slave_write_byte((state >> 8) & 0xff);
-            comms.slave_write_byte((state >> 16) & 0xff);
-            comms.slave_write_byte((state >> 24) & 0xff);
-
-            comms.clear_slave_request();
+        if (process_slave_request()) {
             last_reqest_time = time_us_64();
         }
 
